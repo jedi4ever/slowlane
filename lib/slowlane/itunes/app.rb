a47b863aa472c3384369ffe004015d31f3b37365
Patrick Debois <Patrick.Debois@jedi.be>
2016-05-07 08:43:24 +0200

    format app list & detail for itunes

--- lib/slowlane/itunes/app.rb
+++ lib/slowlane/itunes/app.rb
@@ -1,5 +1,6 @@
 require_relative './util.rb'
 require "spaceship"
+require 'terminal-table'
 
 module Slowlane
   module Itunes
@@ -14,16 +15,53 @@ module Slowlane
 
         t=Utils.team(options)
         Spaceship::Tunes.client.team_id=t
-        
 
+        headings = ['appleId', 'name', 'vendor_id', 'bundle_id', 'last_modified', 'issues_count']
+        rows = []
         Spaceship::Tunes::Application.all.collect do |app|
-          require 'pp' 
-          pp app
-          #pp app.details
+          row = []
+          row << app.apple_id
+          row << app.name
+          row << app.vendor_id
+          row << app.bundle_id
+          row << app.last_modified
+          row << app.issues_count
           #pp app.live_version
-          pp app.edit_version
-          pp app.edit_version.candidate_builds unless app.edit_version.nil?
+          #pp app.edit_version
+          #pp app.edit_version.candidate_builds unless app.edit_version.nil?
+          rows << row
         end
+
+        table = Terminal::Table.new :headings => headings,  :rows => rows
+        puts table
+
+      end
+
+      desc "detail", "details of app <bundle_id>"
+      class_option :team , :default => '<team>' , :required => true
+      def detail(bundle_id)
+
+        c=Utils.credentials(options)
+        Spaceship::Tunes.login(c.username,c.password)
+
+        t=Utils.team(options)
+        Spaceship::Tunes.client.team_id=t
+
+        headings = ['description', 'value' ]
+        rows = []
+        app = Spaceship::Tunes::Application.find(bundle_id)
+        rows << %W(name #{app.name})
+        rows << %W(apple_id #{app.apple_id})
+        rows << %W(bundle_id #{app.bundle_id})
+        rows << %W(last_modified #{app.last_modified})
+        detail = app.details
+        #require 'pp'
+        rows << %W(primary_category #{detail.primary_category})
+        rows << %W(secondary_category #{detail.secondary_category})
+        #pp app.details
+
+        table = Terminal::Table.new :headings => headings,  :rows => rows
+        puts table
 
       end
 
